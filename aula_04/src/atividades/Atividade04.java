ca97630678151496012e19dc2b1ae4a410e371ad
Bryan S. Vieira <bryan.vieira2013@gmail.com>
2023-11-13 06:05:47 -0300

    atividade 4 - aula 4

--- aula_04/src/atividades/Atividade04.java
+++ aula_04/src/atividades/Atividade04.java
@@ -5,33 +5,47 @@ import java.util.Scanner;
 public class Atividade04 {
 
 	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		
-		int idade, ig , pd, mc, hc, nb, mt,hm,outro;
+
+		int idade, ig, pd, back = 0, front = 0, mobile = 0, full = 0, total = 0, somaIdade = 0;
+		float media = 0.0f;
 		String continua = "S";
-		
+
 		while (continua.equalsIgnoreCase("S")) {
 
 			System.out.print("Qual sua idade: ");
 			idade = sc.nextInt();
-			System.out.print("Como se identifica: "
-					+ "1 – Mulher Cis\r\n"
-					+ "2 – Homem Cis\n"
-					+ "3 – Não Binário\n"
-					+ "4 – Mulher Trans\n"
-					+ "5 – Homem Trans\n"
-					+ "6 – Outros\n"
-					+ "ID: ");
+			System.out.print("Como se identifica: " + "\n1 – Mulher Cis\n" + "2 – Homem Cis\n" + "3 – Não Binário\n"
+					+ "4 – Mulher Trans\n" + "5 – Homem Trans\n" + "6 – Outros\n" + "ID: ");
 			ig = sc.nextInt();
-			System.out.println("Qual sua area: "
-					+ "1 – Backend\r\n"
-					+ "2 – Frontend\r\n"
-					+ "3 – Mobile\r\n"
-					+ "4 – FullStack\r\n"
-					+ "ID: ");
+			System.out.print("Qual sua area: " + "\n1 – Backend\n" + "2 – Frontend\n" + "3 – Mobile\n"
+					+ "4 – FullStack\n" + "ID: ");
 			pd = sc.nextInt();
-				if();
-		
+			if (pd == 1)
+				back++;
+			if ((ig == 1 || ig == 4) && pd == 2)
+				front++;
+			if ((ig == 2 || ig == 5) && pd == 3 && idade > 40)
+				mobile++;
+			if (ig == 3 && pd == 4 && idade < 30)
+				full++;
+
+			total++;
+
+			somaIdade += idade;
+
+			media = (float) somaIdade / total;
+
+			System.out.println("Deseja continuar (S/N)");
+			sc.skip("\\R");
+			continua = sc.nextLine().toUpperCase();
 		}
+		System.out.printf("O número de pessoas desenvolvedoras Backend: %d", back);
+		System.out.printf("\nO número de Mulheres Cis e Trans desenvolvedoras Frontend: %d ", front);
+		System.out.printf("\nO número de Homens Cis e Trans desenvolvedores Mobile maiores de 40 anos: %d", mobile);
+		System.out.printf("\nO número de Não Binários desenvolvedores FullStack menores de 30 anos: %d", full);
+		System.out.printf("\nO número total de pessoas que responderam à pesquisa: %d", total);
+		System.out.printf("\nA média de idade das pessoas que responderam à pesquisa: %.2f", media);
 	}
 }
---
+++ aula_04/src/atividades/Atividade05.java
@@ -0,0 +1,14 @@
+package atividades;
+
+import java.util.Scanner;
+
+public class Atividade05 {
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+	
+	int num
+		
+	}
+}
